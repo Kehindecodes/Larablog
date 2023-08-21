dd894a1bc77ca6ab7dfb86042e54a64ba3b4b219
Oki Kehinde <olamiposi.oki@gmail.com>
2023-08-21 13:02:24 +0100

    setup create_post_migration

---
+++ database/migrations/2023_08_21_113946_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->text('content');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('category_id');
+            $table->timestamp('published_at')->nullable();
+
+            // showing relationship between posts and users
+            $table->foreign('user_id')->references('id')->on('users');
+
+            // showing relationship between posts and categories
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
